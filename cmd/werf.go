794d9edfdf4800ba849e45205f249dcaccf0d408
Ilya Lesikov <ilya@lesikov.com>
2024-03-22 19:13:40 +0300

    feat: rename "d8 werf" to "d8 delivery" cmd; remove "d8 delivery kubectl" cmd
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- cmd/werf.go
+++ cmd/werf.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+
 	werfcommon "github.com/werf/werf/cmd/werf/common"
 	werfroot "github.com/werf/werf/cmd/werf/root"
 )
@@ -14,6 +17,20 @@ func init() {
 		werfcommon.TerminateWithError(err.Error(), 1)
 	}
 
+	werfRootCmd.Use = "delivery"
+	removeKubectlCmd(werfRootCmd)
+
 	rootCmd.AddCommand(werfRootCmd)
 	rootCmd.SetContext(ctx)
+}
+
+func removeKubectlCmd(werfRootCmd *cobra.Command) {
+	kubectlCmd, _ := lo.Must2(werfRootCmd.Find([]string{"kubectl"}))
+	kubectlCmd.Hidden = true
+
+	for _, cmd := range kubectlCmd.Commands() {
+		kubectlCmd.RemoveCommand(cmd)
+	}
+
+	werfRootCmd.RemoveCommand(kubectlCmd)
 }
