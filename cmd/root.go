7747c9b05b6c9b7cb8938d194a186fd49d7dd935
Ilya Lesikov <mail@lesikov.com>
2024-04-09 16:18:21 +0300

    Add delivery (werf) subcommand
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,24 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	helm_v3 "helm.sh/helm/v3/cmd/helm"
+
 	cliflag "k8s.io/component-base/cli/flag"
 	"k8s.io/component-base/logs"
+
+	"github.com/werf/logboek"
+	"github.com/werf/nelm/pkg/resrcchangcalc"
+	werfcommon "github.com/werf/werf/cmd/werf/common"
+	"github.com/werf/werf/pkg/process_exterminator"
 )
 
 var Version string
@@ -25,14 +35,40 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+	ctx := rootCmd.Context()
+
 	rand.Seed(time.Now().UnixNano())
 	defer logs.FlushLogs()
 
 	// It is supposed to be executed against the kubectl command, but we want to use this normalization globally.
 	rootCmd.SetGlobalNormalizationFunc(cliflag.WordSepNormalizeFunc)
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
+	if shouldTerminate, err := werfcommon.ContainerBackendProcessStartupHook(); err != nil {
+		werfcommon.TerminateWithError(err.Error(), 1)
+	} else if shouldTerminate {
+		return
 	}
+
+	werfcommon.EnableTerminationSignalsTrap()
+	log.SetOutput(logboek.OutStream())
+	logrus.StandardLogger().SetOutput(logboek.OutStream())
+
+	if err := process_exterminator.Init(); err != nil {
+		werfcommon.TerminateWithError(fmt.Sprintf("process exterminator initialization failed: %s", err), 1)
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		if helm_v3.IsPluginError(err) {
+			werfcommon.ShutdownTelemetry(ctx, helm_v3.PluginErrorCode(err))
+			werfcommon.TerminateWithError(err.Error(), helm_v3.PluginErrorCode(err))
+		} else if errors.Is(err, resrcchangcalc.ErrChangesPlanned) {
+			werfcommon.ShutdownTelemetry(ctx, 2)
+			os.Exit(2)
+		} else {
+			werfcommon.ShutdownTelemetry(ctx, 1)
+			werfcommon.TerminateWithError(err.Error(), 1)
+		}
+	}
+
+	werfcommon.ShutdownTelemetry(ctx, 0)
 }
---
+++ cmd/werf.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+
+	werfcommon "github.com/werf/werf/cmd/werf/common"
+	werfroot "github.com/werf/werf/cmd/werf/root"
+)
+
+func init() {
+	ctx := werfcommon.GetContextWithLogger()
+
+	werfRootCmd, err := werfroot.ConstructRootCmd(ctx)
+	if err != nil {
+		werfcommon.ShutdownTelemetry(ctx, 1)
+		werfcommon.TerminateWithError(err.Error(), 1)
+	}
+
+	werfRootCmd.Use = "d"
+	werfRootCmd.Aliases = []string{"delivery"}
+	removeKubectlCmd(werfRootCmd)
+
+	rootCmd.AddCommand(werfRootCmd)
+	rootCmd.SetContext(ctx)
+}
+
+func removeKubectlCmd(werfRootCmd *cobra.Command) {
+	kubectlCmd, _ := lo.Must2(werfRootCmd.Find([]string{"kubectl"}))
+	kubectlCmd.Hidden = true
+
+	for _, cmd := range kubectlCmd.Commands() {
+		kubectlCmd.RemoveCommand(cmd)
+	}
+
+	werfRootCmd.RemoveCommand(kubectlCmd)
+}
