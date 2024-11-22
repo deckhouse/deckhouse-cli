c806b9ebf80ba092cacb4ac2f159e7f47153a788
Dmitry Denisenko <dmitry.denisenko@flant.com>
2024-11-22 13:51:21 +0300

    ++
    Signed-off-by: Dmitry Denisenko <dmitry.denisenko@flant.com>

--- internal/edit/utilk8s/editconfig.go
+++ internal/edit/utilk8s/editconfig.go
@@ -7,7 +7,6 @@ import (
 	"github.com/deckhouse/deckhouse-cli/internal/backup/utilk8s"
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"log"
 	"os"
 	"os/exec"
 	"sigs.k8s.io/yaml"
@@ -31,12 +30,12 @@ func BaseEditConfigCMD(cmd *cobra.Command, name, secret, dataKey string) error {
 
 	secretConfig, err := kubeCl.CoreV1().Secrets("kube-system").Get(context.Background(), secret, metav1.GetOptions{})
 	if err != nil {
-		log.Fatalf("Error fetching secret: %s", err.Error())
+		return fmt.Errorf("Error fetching secret: %w", err)
 	}
 
 	content, err := yaml.Marshal(secretConfig)
 	if err != nil {
-		log.Fatalf("Error marshaling secret to YAML: %s", err.Error())
+		return fmt.Errorf("Error marshaling secret to YAML: %w", err)
 	}
 
 	type Secret struct {
@@ -46,21 +45,21 @@ func BaseEditConfigCMD(cmd *cobra.Command, name, secret, dataKey string) error {
 	var secretStruct Secret
 	err = yaml.Unmarshal(content, &secretStruct)
 	if err != nil {
-		log.Fatalf("Error parsing YAML file: %v", err)
+		return fmt.Errorf("Error parsing YAML file: %w", err)
 	}
 	decodedValue, err := base64.StdEncoding.DecodeString(secretStruct.Data[dataKey])
 	if err != nil {
-		log.Fatalf("Error decoding base64 value for field '%s': %s", dataKey, err.Error())
+		return fmt.Errorf("Error decoding base64 value for field '%w': %w", dataKey, err)
 	}
 
 	tempFileName, err := os.CreateTemp(os.TempDir(), "secret.*.yaml")
 	if err != nil {
-		log.Fatalf("can't save cluster configuration: %s\n", err)
+		return fmt.Errorf("Can't save cluster configuration: %w\n", err)
 		return err
 	}
 	err = os.WriteFile(tempFileName.Name(), decodedValue, 0644)
 	if err != nil {
-		log.Fatalf("Error writing decoded data to file: %s", err.Error())
+		return fmt.Errorf("Error writing decoded data to file: %w", err)
 	}
 
 	cmdExec := exec.Command(editor, tempFileName.Name())
@@ -69,12 +68,12 @@ func BaseEditConfigCMD(cmd *cobra.Command, name, secret, dataKey string) error {
 	cmdExec.Stderr = os.Stderr
 	err = cmdExec.Run()
 	if err != nil {
-		log.Fatalf("Error opening in editor: %s", err.Error())
+		return fmt.Errorf("Error opening in editor: %w", err)
 	}
 
 	updatedContent, err := os.ReadFile(tempFileName.Name())
 	if err != nil {
-		log.Fatalf("Error reading updated file: %s", err.Error())
+		return fmt.Errorf("Error reading updated file: %w", err)
 	}
 
 	secretData := secretConfig.Data[dataKey]
@@ -87,7 +86,7 @@ func BaseEditConfigCMD(cmd *cobra.Command, name, secret, dataKey string) error {
 
 	_, err = kubeCl.CoreV1().Secrets("kube-system").Update(context.Background(), secretConfig, metav1.UpdateOptions{})
 	if err != nil {
-		log.Fatalf("Error updating secret: %s", err.Error())
+	        return fmt.Errorf("Error updating secret: %w", err)
 	}
 
 	fmt.Println("Secret updated successfully")
