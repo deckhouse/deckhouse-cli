ef0d5439b4e8815203cce6d34f24a8a3954d24c9
Dmitry Denisenko <dmitry.denisenko@flant.com>
2024-11-26 12:58:58 +0300

    ++
    Signed-off-by: Dmitry Denisenko <dmitry.denisenko@flant.com>

---
+++ internal/module/cmd/module-switch/module-switch.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2024 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package module_switch
+
+import (
+	"log"
+	"k8s.io/kubectl/pkg/util/templates"
+	"github.com/spf13/cobra"
+        "github.com/deckhouse/deckhouse-cli/pkg/utilk8s"
+        "github.com/deckhouse/deckhouse-cli/internal/edit/flags"
+)
+
+var clusterConfigurationLong = templates.LongDesc(`
+Edit cluster-configuration in Kubernetes cluster.
+
+© Flant JSC 2024`)
+
+func NewCommand() *cobra.Command {
+	clusterConfigurationCmd := &cobra.Command{
+		Use:           "cluster-configuration",
+		Short:         "Edit cluster-configuration.",
+		Long:          clusterConfigurationLong,
+		SilenceErrors: true,
+		SilenceUsage:  true,
+//		PreRunE:       flags.ValidateParameters,
+		RunE:          editClusterConfig,
+	}
+
+//	flags.AddFlags(clusterConfigurationCmd.Flags())
+	return clusterConfigurationCmd
+}
+
+//func editClusterConfig(cmd *cobra.Command, _ []string) error {
+//	err := utilk8s.BaseEditConfigCMD(cmd, "cluster-configuration", "d8-cluster-configuration", "cluster-configuration.yaml")
+//	if err != nil {
+//		log.Fatalf("Error updating secret: %s", err.Error())
+//	}
+//	return err
+//}
+
+func editClusterConfig(cmd *cobra.Command, _ []string) error {
+
+func(_ *kingpin.ParseContext) error {
+			logger.SetLevel(log.LevelError)
+			cli := kubeclient.New(kubeclient.WithLogger(logger))
+			if err := cli.Init(); err != nil {
+				return err
+			}
+
+			return moduleSwitch(cli, moduleName, true, "enable", logger)
+
+      err := utilk8s.BaseEditConfigCMD(cmd, "cluster-configuration", "d8-cluster-configuration", "cluster-configuration.yaml")
+      if err != nil {
+              log.Fatalf("Error updating secret: %s", err.Error())
+      }
+      return err
+}
+
---
+++ internal/module/module.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2024 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package module
+
+import (
+	"os"
+	"k8s.io/kubectl/pkg/util/templates"
+	"github.com/spf13/cobra"
+	module_switch "github.com/deckhouse/deckhouse-cli/internal/module/cmd/module-switch"
+)
+
+
+var moduleLong = templates.LongDesc(`
+Enable, disable, list enabled, and show modules values in Kubernetes cluster.
+
+© Flant JSC 2024`)
+
+func NewCommand() *cobra.Command {
+	moduleCmd := &cobra.Command{
+		Use: "module", Short: "Edit modules in Kubernetes cluster. ",
+		Long: editLong,
+	}
+
+        moduleCmd.AddCommand(
+                module_switch.NewCommand(),
+        )
+
+        defaultKubeconfigPath := os.ExpandEnv("$HOME/.kube/config")
+        if p := os.Getenv("KUBECONFIG"); p != "" {
+                defaultKubeconfigPath = p
+        }
+	editCmd.PersistentFlags().StringVarP(&defaultKubeconfigPath, "kubeconfig", "k", "", "KubeConfig of the cluster. (default is $KUBECONFIG when it is set, $HOME/.kube/config otherwise)")
+
+	return moduleCmd
+}
