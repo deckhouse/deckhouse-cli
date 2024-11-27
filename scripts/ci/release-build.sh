92442b56f3d31bb2b7d2d4bcbb39506479ff97b8
Dmitry Denisenko <dmitry.denisenko@flant.com>
2024-11-27 18:23:53 +0300

    init
    Signed-off-by: Dmitry Denisenko <dmitry.denisenko@flant.com>

---
+++ scripts/ci/release-build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+VERSION=$1
+if [ -z "$VERSION" ] ; then
+    echo "Required version argument!" 1>&2
+    echo 1>&2
+    echo "Usage: $0 VERSION" 1>&2
+    exit 1
+fi
+
+#task build-and-package
