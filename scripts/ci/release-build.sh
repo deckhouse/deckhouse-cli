e15630537e786ef590412e43f1c2c7e9384478ff
Dmitry Denisenko <dmitry.denisenko@flant.com>
2024-12-12 16:43:28 +0300

    ++
    Signed-off-by: Dmitry Denisenko <dmitry.denisenko@flant.com>

--- scripts/ci/release-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-apt-get update && apt-get install -y apt-utils libbtrfs-dev file git gcc
-sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/bin
-git config --global url."https://gitlab-ci-token:$PRIVATE_REPO_TOKEN@fox.flant.com/".insteadOf https://fox.flant.com/
-git config --global --add safe.directory '*'
