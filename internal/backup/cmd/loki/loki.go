045a77de88e22d88e0f7de139cc080fa90c8d1a0
dmitry.denisenko <dmitry.denisenko@flant.com>
2025-02-12 20:08:17 +0400

    ++

--- internal/backup/cmd/loki/loki.go
+++ internal/backup/cmd/loki/loki.go
@@ -208,7 +208,7 @@ func backupLoki(cmd *cobra.Command, _ []string) error {
 		for _, pod := range resultLog.Stream.Pod {
 			for _, log := range resultLog.Values {
 				//fmt.Sprintf("Pod: %s\nTimestamp: %s, Log: %s\n", pod, log[0], log[1])
-				fmt.Printf("Pod: %s\nTimestamp: %s, Log: %s\n", pod, log[0], log[1])
+				fmt.Printf("Pod: %v\nTimestamp: %s, Log: %s\n", pod, log[0], log[1])
 			}
 		}
 	}
