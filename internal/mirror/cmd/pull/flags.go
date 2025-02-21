235354ecd62d68f73fc9b4b0143430977c66479d
borg-z <me@nikolay-z.top>
2025-02-21 13:33:28 +0300

    d8 mirror pull/push --tmp-dir flag
    Signed-off-by: borg-z <me@nikolay-z.top>

--- internal/mirror/cmd/pull/flags.go
+++ internal/mirror/cmd/pull/flags.go
@@ -18,6 +18,7 @@ package pull
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/pflag"
 )
@@ -141,5 +142,11 @@ func addFlags(flagSet *pflag.FlagSet) {
 		"insecure",
 		false,
 		"Interact with registries over HTTP.",
+	)
+	flagSet.StringVar(
+		&TempDir,
+		"tmp-dir",
+		filepath.Join(os.TempDir(), "d8", "mirror"),
+		"Temporary directory to use for image pulling",
 	)
 }
