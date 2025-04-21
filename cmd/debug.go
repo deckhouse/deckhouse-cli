08a9c27a3798372ebded1deafc7397f10b734095
Denis Tarabrin <denis.tarabrin@flant.com>
2025-04-21 16:43:04 +0400

    go

--- cmd/debug.go
+++ cmd/debug.go
@@ -7,7 +7,6 @@ import (
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/kubectl/pkg/cmd/debug"
-	"k8s.io/kubectl/pkg/cmd/util"
 )
 
 func init() {
@@ -24,39 +23,35 @@ func init() {
 	originalDebugCmd = ReplaceCommandName("kubectl", "d8", originalDebugCmd)
 
 	debugCmd := &cobra.Command{
-		Use:     originalDebugCmd.Use,
-		Short:   originalDebugCmd.Short,
-		Long:    originalDebugCmd.Long,
-		Example: originalDebugCmd.Example,
-		RunE: func(cmd *cobra.Command, args []string) error {
+		Use:                originalDebugCmd.Use,
+		Short:              originalDebugCmd.Short,
+		Long:               originalDebugCmd.Long,
+		Example:            originalDebugCmd.Example,
+		DisableFlagParsing: true,
+		Run: func(cmd *cobra.Command, args []string) {
 			hasImage := false
-			hasCopyTo := false
 			for _, arg := range args {
 				if strings.HasPrefix(arg, "--image=") || arg == "--image" {
 					hasImage = true
 					break
 				}
-				if strings.HasPrefix(arg, "--copy-to=") || arg == "--copy-to" {
-					hasCopyTo = true
-					break
-				}
 			}
 
-			if !hasImage && !hasCopyTo {
+			if !hasImage {
 				args = append(args, fmt.Sprintf("--image=%s", defaultImage))
 				fmt.Fprintf(cmd.OutOrStdout(), "Using default image: %s\n", defaultImage)
 			}
 
+			originalDebugCmd.DisableFlagParsing = false
 			originalDebugCmd.SetArgs(args)
-			return originalDebugCmd.Execute()
+			if err := originalDebugCmd.Execute(); err != nil {
+				fmt.Fprintf(cmd.ErrOrStderr(), "Error: %v\n", err)
+			}
 		},
 	}
 
 	debugCmd.Flags().AddFlagSet(originalDebugCmd.Flags())
-
 	configFlags.AddFlags(debugCmd.Flags())
-
-	util.AddPodRunningTimeoutFlag(debugCmd, 0)
 
 	if imageFlag := debugCmd.Flags().Lookup("image"); imageFlag != nil {
 		imageFlag.Usage = "Container image to use for debug container. If not specified, nicolaka/netshoot will be used."
