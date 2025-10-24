7be2ba2f4a908b1ddd9c0999055b2270c4477abe
Slava V <vyacheslav.voytenok@flant.com>
2025-10-25 00:21:38 +0700

    remove unused import
    Signed-off-by: Slava V <vyacheslav.voytenok@flant.com>

--- internal/dataimport/util/util.go
+++ internal/dataimport/util/util.go
@@ -13,7 +13,6 @@ import (
 	"github.com/deckhouse/deckhouse-cli/internal/dataimport/api/v1alpha1"
 	"github.com/deckhouse/deckhouse-cli/internal/dataio"
 	safeClient "github.com/deckhouse/deckhouse-cli/pkg/libsaferequest/client"
-	"github.com/deckhouse/deckhouse/pkg/log"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrlrtclient "sigs.k8s.io/controller-runtime/pkg/client"
