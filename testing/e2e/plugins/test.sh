54691c6ac8c70340a7bd57f28f17b58b036a3b10
Smyslov Maxim <maksim.smyslov@flant.com>
2026-03-03 16:04:11 +0300

    plugins test script
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

---
+++ testing/e2e/plugins/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Prerequirements:
+# - login to registry
+# - d8 binary in ./bin/ (run task build)
+
+# Inputs:
+# ARG1 = registry login
+# ARG2 = registry password
+
+# setup
+export DECKHOUSE_CLI_PATH="$(PWD)/e2e.test"
+export DECKHOUSE_REGISTRY_REPO="dev-registry.deckhouse.io/deckhouse/foxtrot"
+export D8_MIRROR_SOURCE_LOGIN="license-token"
+export D8_MIRROR_SOURCE_PASSWORD=$1
+
+# cleanup 
+cleanup_on_error() {
+    rm -rf ./e2e.test/
+}
+# Defer the removal of the temp file to the script's exit
+trap cleanup_on_error EXIT ERR
+
+# tests
+echo "--- TEST LIST PLUGINS ---"
+$(PWD)/bin/d8 plugins list # run task build before required 
+
+echo ""
+echo "--- TEST CLEAN INSTALL PLUGIN ---"
+$(PWD)/bin/d8 plugins install package
+
+echo ""
+echo "--- TEST INSTALL SECOND PLUGIN ---"
+$(PWD)/bin/d8 plugins install system
